476493d11ef641a0662b6880a56895b9aaa0850a
PSewmuthu <apsewmuthu@gmail.com>
2025-02-21 20:45:28 +0530

    Add CourseSelectionAgent class and show_menu() in it

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ https://github.com/PSewmuthu/Multi-Agent_Student_Registration_System.git
 
 class StudentRegistrationAgent:
     def __init__(self):
+        '''
+        Student Registration Agent: Please enter your name and student ID. 
+        Student: John Doe, 12345 
+        Student Registration Agent: Welcome, John Doe (ID: 12345). Proceeding to course selection.
+        '''
+
         inpts = []
 
         while True:
@@ -24,3 +30,28 @@ class StudentRegistrationAgent:
     def registration(self, name, id):
         print(
             f"\nStudent Registration Agent: Welcome, {name} (ID: {id}). Proceeding to course selection.\n\n")
+
+
+class CourseSelectionAgent:
+    def __init__(self):
+        self.courses = {
+            'Data Structures': 300,
+            'Algorithms': 350,
+            'Machine Learning': 400
+        }
+
+    def show_menu(self):
+        '''
+        Course Selection Agent: Available courses are: 
+        1. Data Structures - $300 
+        2. Algorithms - $350 
+        3. Machine Learning - $400 
+        Please type the courses you want to register for (separate by commas).
+        '''
+
+        print("Course Selection Agent: Available courses are:")
+
+        for ind, (name, price) in enumerate(self.courses.items()):
+            print(f"{ind}. {name}\t - \t${price}")
+
+        print("Please type the courses you want to register for (separate by commas).")
